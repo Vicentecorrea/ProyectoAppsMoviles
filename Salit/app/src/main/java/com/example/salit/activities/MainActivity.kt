ce80fc68f3bf172c5c7ed14e6c175fd7b5068a14
Vicente Correa <vicorrea@miuandes.cl>
2019-06-12 12:38:10 -0400

    puCH

--- Salit/app/src/main/java/com/example/salit/activities/MainActivity.kt
+++ Salit/app/src/main/java/com/example/salit/activities/MainActivity.kt
@@ -2,12 +2,81 @@ package com.example.salit.activities
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v4.app.Fragment
+import android.support.v4.view.GravityCompat
+import android.view.MenuItem
 import com.example.salit.R
+import com.example.salit.fragments.HomeFragment
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    private var currentLoadedFragment: Fragment? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setToolbar()
+    }
+
+    private fun setToolbar() {
+        setSupportActionBar(toolbar)
+        supportActionBar?.apply {
+            setDisplayHomeAsUpEnabled(true)
+//            setHomeAsUpIndicator(R.drawable.ic_menu_black_24dp)
+        }
+        setupNavViewListener()
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun setupNavViewListener() {
+        navView.setNavigationItemSelectedListener { menuItem ->
+            // set item as selected to persist highlight
+            menuItem.isChecked = true
+            // close drawer when item is tapped
+            drawerLayout.closeDrawers()
+
+            // Add code here to update the UI based on the item selected
+            // For example, swap UI fragments here
+            val transaction = supportFragmentManager.beginTransaction()
+
+            when (menuItem.itemId) {
+                R.id.home -> {
+                    val homeFragment = supportFragmentManager.findFragmentByTag("homeFrag")
+                    if (homeFragment != null) {
+                        transaction.replace(R.id.contentFrameLayout, homeFragment)
+                    } else {
+                        transaction.replace(R.id.contentFrameLayout, HomeFragment(), "homeFrag")
+                    }
+                    supportActionBar!!.title = getString(R.string.action_bar_home_title)
+                }
+
+//                R.id.makeComplain -> {
+//                    val makeComplaintFragment = supportFragmentManager.findFragmentByTag("createComplaintFrag")
+//                    if (makeComplaintFragment != null) {
+//                        transaction.replace(R.id.contentFrameLayout, makeComplaintFragment)
+//                    } else {
+//                        transaction.replace(R.id.contentFrameLayout, MakeComplaintFragment(), "createComplaintFrag")
+//                    }
+//                    supportActionBar!!.title = getString(R.string.action_bar_make_complaint_title)
+//                }
+
+
+//                R.id.signOut -> {
+//                    onSignOut()
+//                }
+            }
+
+            transaction.commit()
+            true
+        }
     }
 }
